1a222166d5466dd316c15a4fbc8cb39e945211c5
0xcatfish <165363031+0xcatfish@users.noreply.github.com>
2024-03-29 13:35:34 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-65d26f19-c7a4-4cbb-aabe-3a93c76519a9/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-5ee1148a-73e9-4578-abcf-60fc731efad8/)
 
 
 echo "$curl_output" >> Update.tsx
